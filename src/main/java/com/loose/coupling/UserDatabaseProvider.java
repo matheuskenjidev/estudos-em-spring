aec2373a9fe7af08ebfc2ea4dbc03fa1700b91fd
Matheus Kenji <matheuskenjidev@gmail.com>
2026-08-15 22:08:32 -0300

    Loose coupling and Tight coupling

---
+++ src/main/java/com/loose/coupling/UserDatabaseProvider.java
@@ -0,0 +1,9 @@
+package com.loose.coupling;
+
+public class UserDatabaseProvider implements UserDataProvider{
+    @Override
+    public String getUserDetails() {
+        //directly access database here
+        return "User Details from Database";
+    }
+}
